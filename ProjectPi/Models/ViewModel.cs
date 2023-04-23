1d0973239f2a66f645f5943c1a1973be9a96644e
PlowRain138 <plowrain1328@gmail.com>
2023-04-23 18:37:06 +0800

    fix:Chat

--- ProjectPi/Models/ViewModel.cs
+++ ProjectPi/Models/ViewModel.cs
@@ -122,6 +122,28 @@ namespace ProjectPi.Models
 
         }
 
+        /// <summary>
+        /// 已讀對方訊息的VIEW
+        /// </summary>
+        public class PostReadChatRooms
+        {
+            /// <summary>
+            /// 個案編號
+            /// </summary>
+            [Display(Name = "個案編號")]
+            public int UserId { get; set; }
+            /// <summary>
+            /// 諮商師編號
+            /// </summary>
+            [Display(Name = "諮商師編號")]
+            public int CounselorId { get; set; }
+            /// <summary>
+            /// 用戶類別
+            /// </summary>
+            [Display(Name = "用戶類別")]
+            public string MyType { get; set; }
+        }
+
 
     }
 }
